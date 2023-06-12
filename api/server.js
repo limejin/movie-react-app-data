86a47c29a0f421ec3cfbc756932001590f3f94be
limejin <91420946+limejin@users.noreply.github.com>
2023-06-13 03:39:32 +0900

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,7 @@
 //See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const server = jsonServer.create();
-const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
-// const server = jsonServer.create()
-// const router = jsonServer.router('db.json')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
@@ -16,13 +14,6 @@ server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
-const fs = require("fs");
-const os = require("os");
-
-fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
-  if (err) console.log(err);
-  else console.log("copy file succeed to" + os.tmpdir());
-});
 
 
 // Export the Server API
