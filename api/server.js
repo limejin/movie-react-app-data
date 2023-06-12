9778218b9b9e587df3063d0be59d5e412421c6a2
limejin <91420946+limejin@users.noreply.github.com>
2023-06-13 03:34:13 +0900

    update: 서버 수정

--- api/server.js
+++ api/server.js
@@ -1,19 +1,28 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+// server.use(middlewares)
+// // Add this before server.use(router)
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/blog/:resource/:id/show': '/:resource/:id'
+// }))
+// server.use(router)
+// server.listen(3000, () => {
+//     console.log('JSON Server is running')
+// })
+const fs = require("fs");
+const os = require("os");
 
+fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+  if (err) console.log(err);
+  else console.log("copy file succeed to" + os.tmpdir());
+});
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
 // Export the Server API
 module.exports = server
